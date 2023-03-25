b944c0d195364c71258907d59872211ff06e0c3e
Roberto Chambi <rcchambi4@umsa.bo>
2023-03-25 18:41:51 -0400

    Estilos con jquery 1

--- js/estilos.js
+++ js/estilos.js
@@ -20,4 +20,16 @@ $(document).ready(()=>{
   console.log("El color de texto es: ", color);
   console.log("El margin-top es: ", $('.boton-rojo').css('margin-top'));
   console.log("El margin-bottom es: ", $('.boton-rojo').css('margin-bottom'));
+
+  // Asignar un valor a la propiedad CSS de un elemento
+  $('.boton-azul').css('border','1px solid red');
+
+  // Asignar valores a mas de una propiedad CSS
+  // $('.boton-azul').css({'propiedad':'valor', 'propiedad': 'valor'})
+
+  // Cambiar dimensiones width height inner outer
+  $('.caja').width('300px');
+  // $('.caja').outerWidth('80px');
+  $('.caja').css('border', '4px solid yellow')
+  console.log($('.caja').css('width'));
 })
