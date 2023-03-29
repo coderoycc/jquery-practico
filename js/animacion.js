389a7351aebb7f1dd99865524ae85db3d522a97e
Roberto Chambi <rcchambi4@umsa.bo>
2023-03-28 20:24:33 -0400

    Añadido de animaciones con jquery

---
+++ js/animacion.js
@@ -0,0 +1,32 @@
+$(document).ready(()=>{
+  // animacion 
+  /* $('button').click(()=>{
+    //verifica estado y lo oculta o muestra
+    $('div').toggle(800) // se le puede dar un tiempo en MS
+  }); */
+
+  // DESVANECIMIENTO
+  // tambien se puede usar fadeIn() Mostrar
+  //  o fadeOut() Ocultar
+  // Para ambos fadeToggle()
+  // A cada uno se le puede asignar un tiempo
+
+  // DESLISAMIENTO
+  // slideUp() para ocultar
+  // slideDown() para mostrar
+  // slideToggle() para ambos
+
+
+  // cambiando propiedades
+  $('button').click(()=>{
+    $('div').animate({
+      'margin':'0 auto',
+      'width':'250px',
+      'height':'500px'
+    },1500);
+    // Reducir cumulativamente en cada click
+    // 'width':'-=50px'
+    // Aumentar
+    // 'width':'+=5px'
+  })
+});
